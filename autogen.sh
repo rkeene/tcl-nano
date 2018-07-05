77de64dbef5e7fe50ff7e5a501d2c1ae25c6a7bd
rkeene <rkeene@noemail.net>
2018-07-05 15:02:24 +0000

    Improvements to handling of patched dependencies
    FossilOrigin-Name: b90a759587245e85b1a0b2a9d5a4325843f87e8bc19addcdcd62847dcc85e00e

--- autogen.sh
+++ autogen.sh
@@ -80,13 +80,9 @@ rm -rf autom4te.cache
 set -e
 
 # Assemble tweetnacl
-make -C build/tweetnacl
 rm -rf tweetnacl
-mkdir tweetnacl
-cp build/tweetnacl/out/* tweetnacl/
+make -C build/tweetnacl install PREFIX="$(pwd)/tweetnacl"
 
 # Assemble blake2b
-make -C build/blake2b
 rm -rf blake2b
-mkdir blake2b
-cp build/blake2b/out/* blake2b/
+make -C build/blake2b install PREFIX="$(pwd)/blake2b"
