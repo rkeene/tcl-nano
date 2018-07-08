87d8e3d7ecd5b996f7c5c8cd6a5b054672ce49ca
rkeene <rkeene@noemail.net>
2018-07-08 21:39:23 +0000

    Remove mrproper from distributed Makefile, since it is only for internal use
    FossilOrigin-Name: a0d2d37cfeecd941098f8cabbe60a3b14a9fd5c0d386e27de80714568ad5135a

--- build/pre.sh
+++ build/pre.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/env bash
 
-./autogen.sh || exit 1
+set -e
+./autogen.sh
 rm -rf aclocal
+
+cat Makefile.in  | sed '/^mrproper:/,/^$/ d' > Makefile.in.new
+cat Makefile.in.new > Makefile.in
+rm -f Makefile.in.new
 
 exit 0
